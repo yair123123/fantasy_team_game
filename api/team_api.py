116ef5d8cf916854c6d6838b765dcae68ad4e9d5
Yair <ytykhn8@gmail.com>
2024-09-19 18:57:01 +0300

    add func api

--- api/team_api.py
+++ api/team_api.py
@@ -13,4 +13,9 @@ def get_players_by_year(year:int) -> List[Dict[str, str]]:
     # if questions_and_answers['response_code'] == 5:
     #     return get_questions_and_answers()
     return players
+def get_all_players() -> List[Dict[str, str]]:
+    players = get_from_api("http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataTotals/query?season=2022&&pageSize=100")
+    # if questions_and_answers['response_code'] == 5:
+    #     return get_questions_and_answers()
+    return players
 
